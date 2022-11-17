d34d3e91b9ebb580f28bdef8d9eb237c37e63f23
joshuacedwards1 <116026525+joshuacedwards1@users.noreply.github.com>
2022-11-16 21:32:28 -0500

    Create sum function that adds two numbers

--- javascript-basics/javascript.js
+++ javascript-basics/javascript.js
@@ -1,10 +1,19 @@
 // This is my first javascript comment!
 /* This is my first multi-line
 comment! */
+
 let myFirstName = "Joshua";
 let myLastName = "Edwards";
+
 const wifeFirstName = "Alyssia";
 const wifeMiddleName = "Joelle";
 const wifeLastName = "Edwards";
+
 let couple = myFirstName + " " + myLastName + " and " + wifeFirstName + " " + wifeLastName;
 console.log(couple);
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(sum(10, 15));
